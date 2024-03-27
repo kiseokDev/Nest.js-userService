a0445a0d45217d314e30cc371bc80c2a66b3c14d
kiseokDev <dev.leekiseok@gmail.com>
2024-03-27 23:53:54 +0900

    nextjs 빌드 코드 수정, wsl2 운영체제용으로 플랫폼 바꾸기(docker-compose)

--- apps/nextjs-front/src/lib/envConstant.ts
+++ apps/nextjs-front/src/lib/envConstant.ts
@@ -1,4 +1,4 @@
 export const BACKEND_CONFIG = {
-  BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:4000',
+  BACKEND_URL: process.env.BACKEND_URL || 'http://localhost:3001',
   ENV: process.env.ENV || 'development',
 };
