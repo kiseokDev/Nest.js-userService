9fabca3eac5eed7b27e7aaa74a8163d3b79fe57d
kiseokDev <dev.leekiseok@gmail.com>
2024-04-30 00:48:34 +0900

    test: stock 크롤링 샘플링

--- apps/nestjs-back/src/stocks/stocks.service.ts
+++ apps/nestjs-back/src/stocks/stocks.service.ts
@@ -18,12 +18,25 @@ export class StocksService {
 
   async stockTest() {
     const page = await this.browser.newPage();
-    await page.goto('https://www.google.com');
-    console.log('browser open');
+
+    // 네트워크 연결이 2개 이하가 되면 페이지 로드가 완료된 것으로 간주하고, 최대 60초까지 기다립니다.
+    await page.goto('https://www.choicestock.co.kr/search/summary/TSLA', {
+      waitUntil: 'networkidle2',
+      timeout: 60000, // 타임아웃을 60초로 설정
+    });
+
+    try {
+      await page.waitForSelector('.dataBox', { timeout: 10000 }); // 60초 동안 기다림
+      const content = await page.evaluate(() => {
+        const element = document.querySelector('.dataBox');
+        return element ? element.innerHTML : null;
+      });
+      console.log(content);
+    } catch (error) {
+      this.browser.close();
+    }
   }
-  getBrowser() {
-    return this.browser;
-  }
+  getBrowser() {}
   setBrowser(browser: Browser) {
     this.browser = browser;
   }
